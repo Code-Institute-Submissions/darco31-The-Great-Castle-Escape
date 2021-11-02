4153e93398f92ba289e24c7d0fa1f0cc6bdaef9a
Stephen <sdarcy29@hotmail.com>
2021-11-02 20:37:33 +0000

    Initial commit add function.py and import statements for time and colorama

---
+++ functions.py
@@ -0,0 +1,10 @@
+import time
+
+
+def P_STAT(text, delay):
+    """
+    To print a line of text and delay the next line for a set amount of time
+    """
+    print(text)
+    time.sleep(delay)
+
--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Creator: Stephen Darcy
+# Date:
+# Project 3 - The code Institute
+
+from functions import P_STAT
+import time
+import colorama
+from colorame import Fore, Back, Style
+colorama.init(autoreset=True)
+
+
