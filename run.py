0c8a4049622f793f46b632e55462ded7378d030e
Stephen <sdarcy29@hotmail.com>
2021-11-03 20:31:07 +0000

    Add new functions to proceed to other areas such as rom to right and down the staircase

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ TIME_ELAPSED = 2
 
 # Start the game and gives initial choices to the player
 
+
 def start():
     """
     Starts the game, gives a narrative to set the scene and asks
@@ -80,12 +81,12 @@ def small_window():
     and decides next course of action
     """
     P_STAT("\n You get up and walk towards the window", 1)
-    P_STAT("\n You peer out an can only see darkeness", 1)
+    P_STAT("\n You peer out an can only see darkness", 1)
     P_STAT("\n Do you try open the window? Y or N", 2)
 
     try_window = input("=> \n").lower().strip()
     if try_window == "y" or try_window == "yes":
-        P_STAT("\n You try the wondow, nothing.", 1)
+        P_STAT("\n You try the window, nothing.", 1)
         P_STAT("\n Urggh its sealed shut", 1)
         try_door()
     else:
@@ -98,7 +99,7 @@ def try_door():
     The player approcahes the large door and is offered
     various choices in which way to go
     """
-    P_STAT("\n Reaching the door tou try the handle", 2)
+    P_STAT("\n Reaching the door you try the handle", 2)
     P_STAT("\n It opens, how odd you think", 2)
     P_STAT("\n Do you take a look outside? (Y or N)", 2)
 
@@ -183,17 +184,114 @@ def direction_choice():
     Player will make a choice to go left or right
     out of the doorway.
     """
-    "\n Standing in the doorway you need to make a choice", 1
-    "\n Do you go left or go right? (Left or Right)", 2
+    P_STAT("\n Standing in the doorway you need to make a choice", 1)
+    P_STAT("\n Do you go left or go right? (Left or Right)", 2)
 
     player_choice = input("=> \n").lower().strip()
 
     if player_choice == "left":
-        go-left()
+        go_left()
     else:
         go_right()
 
-# Section 2 player goes left or right
+# Section 2 player goes left
+
+
+def go_left():
+    """
+    The player goes left and will have to makes choices
+    in the next room
+    """
+    P_STAT("\n You decide to turn left and head towards the door", 2)
+    P_STAT("\n As you approach the door you slow down", 2)
+    P_STAT("\n You push at the door and it creaks open", 2)
+    P_STAT("\n As your eyes adjust you make out a single candle light", 2)
+    P_STAT("\n You walk inside, at the far end there is an opening", 2)
+    P_STAT("\n You approach the opening and see a staircase going down", 2)
+    P_STAT("\n You look around the room", 2)
+    P_STAT("\n Do you explore ,proceed down stairs or go back?", 2)
+
+    decision = input("=> \n").lower().strip()
+
+    if decision == "explore":
+        explore_room()
+    elif decision == "proceed":
+        proceed_down_stairs()
+    else:
+        P_STAT("\n You decide it would be better to check the other door", 2)
+        go_right()
+
+
+def explore_room():
+    """
+    Player decides to explore room
+    """
+    P_STAT("\n As you look around the room the idea you are in a castle", 2)
+    P_STAT("\n still leaves you feeling confused", 2)
+    P_STAT("\n You see a bowl with fruit in it, you are starving so", 2)
+    P_STAT("\n pick it up and eat while continuing to explore", 2)
+    P_STAT("\n You find a belt, ", 2)
+    P_STAT("\n you wrap it around you and slip the knife in", 2)
+    P_STAT("\n As there is nothing else left to check ", 2)
+    P_STAT("\n you decide to go down stairs", 2)
+
+    proceed_down_stairs()
+
+
+def proceed_down_stairs():
+    """
+    Player proceeds down the spiral stairs
+    """
+    P_STAT("\n You enter the doorway", 2)
+    P_STAT("\n Hugging the wall you make your way down", 2)
+    P_STAT("\n You reach the bottom and as you listen", 2)
+    P_STAT("\n you can hear what appears to be two voices", 2)
+    P_STAT("\n You take a breath and peek around the corner and see", 2)
+    P_STAT("\n two large men with what looks like swords?", 2)
+    P_STAT("\n Their backs are to you", 2)
+    P_STAT("\n Hmmm attack or sneak? Your choice - Attack or Sneak", 2)
+
+    attack = input("=> \n").lower().strip()
+
+    if attack == "attack":
+        P_STAT("\n You charge at the men who are surprised", 2)
+        P_STAT("\n But its too little, one fouls swipe and you are....", 1)
+
+        P_STAT(Fore.RED + '''
+                              ╔╗           ╔╗
+                              ║║           ║║
+                            ╔═╝║╔══╗╔══╗ ╔═╝║
+                            ║╔╗║║╔╗║╚ ╗║ ║╔╗║
+                            ║╚╝║║║═╣║╚╝╚╗║╚╝║
+                            ╚══╝╚══╝╚═══╝╚══╝
+            ''', 2)
+        play_again()
+
+    else:
+        P_STAT("\n You put your back against the wall", 2)
+        P_STAT("\n and sneak as quietly as you can passed them", 2)
+
+
+def go_right():
+    """
+    Player returns to the corridor and goes to the right
+    hand door from the starting room. This will also be the go right from the 
+    room the player initially makes.
+    """
+    P_STAT("\n You decided to go back from the room with the stairs", 2)
+    P_STAT("\n You reach the door and try open it", 2)
+    P_STAT("\n You then remebr the key and try it, it works", 2)
+    P_STAT("\n You push the door open and enter", 2)
+    P_STAT("\n Loking around this lavish room you notice what looks like", 2)
+    P_STAT("\n three levers on the wall", 2)
+    P_STAT("\n Do you try the levers? (Y or N", 1)
+
+    try_lever = input("=> \n").lower().strip()
+
+    if try_lever == "y" or try_lever == "yes":
+        P_STAT("\n You try the first lever, nothing happens", 1.2)
+        P_STAT("\n Maybe its a combination of the levers", 1.2)
+        P_STAT("\n Worth a go? (Y or N)", 2)
 
 
 def play_again():
